5f59dbcd2ca9b626ca5d33202f71c8e76f326a11
Sheralram <ramkrishna.sheral@gmail.com>
2021-12-01 18:36:42 +0530

    Added : Hello Mark from Bridgelabz message by GET Request Method And Pass Name as Path Variable

--- src/main/java/com/bridgelabz/springapphellobridgelabz/controller/HelloWebController.java
+++ src/main/java/com/bridgelabz/springapphellobridgelabz/controller/HelloWebController.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.springapphellobridgelabz.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,4 +20,10 @@ public class HelloWebController {
         return "Hello " + name + " from Bridgelabz";
     }
 
+    @GetMapping("/test1/{id}")
+    public String test1(
+            @PathVariable(name = "id") String id
+    ) {
+        return "Hello " + id + " from Bridgelabz";
+    }
 }
